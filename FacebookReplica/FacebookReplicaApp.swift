177e8f2c61fa3d6aec10e0b4afba3d895ea01ecc
Chaudhry Umair <chaudhryumair007@yahoo.com>
2024-05-14 03:22:13 +0500

    Initial Commit

---
+++ FacebookReplica/FacebookReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookReplicaApp.swift
+//  FacebookReplica
+//
+//  Created by Chaudhry Umair on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FacebookReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
